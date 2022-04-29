07d722590c4896f0de71b92187343d320c0c10dc
muhammedaltug <muhammetaltug@gmail.com>
2022-04-29 12:09:10 +0300

    add usage example

--- app/angular/src/app/home/home.component.ts
+++ app/angular/src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { AuthService } from '@abp/ng.core';
 import { Component } from '@angular/core';
 import { OAuthService } from 'angular-oauth2-oidc';
+import { SimpleModuleADtosService } from 'module-a';
+import { SimpleModuleBDtosService } from 'module-b';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +14,15 @@ export class HomeComponent {
     return this.oAuthService.hasValidAccessToken();
   }
 
-  constructor(private oAuthService: OAuthService, private authService: AuthService) {}
+  constructor(
+    private oAuthService: OAuthService,
+    private authService: AuthService,
+    private simpleModuleBDtosService: SimpleModuleBDtosService,
+    private simpleModuleADtosService: SimpleModuleADtosService
+  ) {
+    this.simpleModuleADtosService.getDto1().subscribe();
+    this.simpleModuleBDtosService.getDto3().subscribe();
+  }
 
   login() {
     this.authService.navigateToLogin();
